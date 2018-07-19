0468a57495a9b798d6597f61c7cb226e6c3c7bb6
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-19 13:57:36 -0300

    FlexBox

--- Modulo01/src/index.js
+++ Modulo01/src/index.js
@@ -11,30 +11,6 @@ import Todo from './components/Todo'
 
 export default class App extends Component {
 
-  // CICLO DE VIDA
-  // Primeiro método chamado na tela, logo que é criada
-  componentDidMount() { }
-
-
-  // Executado antes do componentDidMount e antes de cada atualização
-  // Apenas se tiver alguma variável no state que dependa de algum valor das props
-  static getDerivedStateFromProps(nextProps, prevState){
-    return {text: nextProps.text} //passando o text das props para o state
-  }
-
-  //executado a cada atualização, e decide se o componente deve atualizar ou não
-  // retorna boolean
-  shouldComponentUpdate(nextProps, nextState){
-    return nextState.counter < 5
-  }
-
-  //executado depois de executar o render
-  componentDidUpdate(prevProps, prevState){ }
-
-
-  //antes do componente ser desmontado
-  componentWillUnmount(){ }
-
   state ={
     usuario: 'Wesley',
     todos: [
@@ -52,13 +28,11 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <Text>{this.state.usuario}</Text>
-        {
-          this.state.todos.map((todo) => (
-            <Todo key={todo.id} title={todo.text} />
-          ))
-        }
-        <Button title="Adicionar Todo" onPress={this.addTodo} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
       </View>
     );
   }
@@ -67,8 +41,20 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#333',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: '#f5f5f5',
+    alignContent: 'center',
   },
+  box: {
+    width: 80,
+    height: 80,
+    backgroundColor: '#F00',
+    margin: 10,
+    transform: [
+      { rotateZ: '20deg'}
+    ]
+  }
 });
