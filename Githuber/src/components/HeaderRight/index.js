15b42547cc794b7e0fc1e01c3e1363e353028bf7
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-19 20:04:58 -0300

    Logout no header

--- Githuber/src/components/HeaderRight/index.js
+++ Githuber/src/components/HeaderRight/index.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+import PropTypes from 'prop-types';
+
+import { TouchableOpacity, AsyncStorage } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
 
 
 export default class HeaderRight extends Component {
-  signOut = () => {
-    console.log('sair');
+  static propTypes = {
+    navigation: PropTypes.shape({
+      dispatch: PropTypes.func,
+    }).isRequired,
+  }
+
+  signOut = async () => {
+    await AsyncStorage.clear();
+
+    const resetAction = NavigationActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'Welcome' }),
+      ],
+    });
+
+    const { navigation } = this.props;
+
+    navigation.dispatch(resetAction);
   }
 
   render() {
