ba1dca1455fb782f3f6a81c08e531311bc190577
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-19 13:47:05 -0300

    LifeCycles

--- Modulo01/src/index.js
+++ Modulo01/src/index.js
@@ -11,6 +11,30 @@ import Todo from './components/Todo'
 
 export default class App extends Component {
 
+  // CICLO DE VIDA
+  // Primeiro método chamado na tela, logo que é criada
+  componentDidMount() { }
+
+
+  // Executado antes do componentDidMount e antes de cada atualização
+  // Apenas se tiver alguma variável no state que dependa de algum valor das props
+  static getDerivedStateFromProps(nextProps, prevState){
+    return {text: nextProps.text} //passando o text das props para o state
+  }
+
+  //executado a cada atualização, e decide se o componente deve atualizar ou não
+  // retorna boolean
+  shouldComponentUpdate(nextProps, nextState){
+    return nextState.counter < 5
+  }
+
+  //executado depois de executar o render
+  componentDidUpdate(prevProps, prevState){ }
+
+
+  //antes do componente ser desmontado
+  componentWillUnmount(){ }
+
   state ={
     usuario: 'Wesley',
     todos: [
