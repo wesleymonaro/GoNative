b320f9a74a547f6ce5ae02998e777061c5a46c04
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-19 13:05:09 -0300

    Props

---
+++ Modulo01/src/components/Todo.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import {View, Text, StyleSheet} from 'react-native';
+
+export default class Todo extends Component{
+    render(){
+        return(
+            <View>
+                <Text>{this.props.title}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+})
--- Modulo01/src/index.js
+++ Modulo01/src/index.js
@@ -1,38 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Todo from './components/Todo'
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to GoNative!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Todo title="Fazer café"/>
       </View>
     );
   }
@@ -43,16 +22,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#f5f5f5',
   },
 });
