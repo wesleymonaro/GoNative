72407cefc385731b75b8c8fad4de3f4a834d1b10
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-19 14:18:22 -0300

    ESLint

--- Modulo01/src/index.js
+++ Modulo01/src/index.js
@@ -1,42 +1,8 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  Text,
   View,
-  Button
 } from 'react-native';
-
-import Todo from './components/Todo'
-
-
-export default class App extends Component {
-
-  state ={
-    usuario: 'Wesley',
-    todos: [
-      { id: 0, text: 'Fazer café'},
-      { id: 1, text: 'Estudar o GoNative'},
-    ]
-  }
-
-  addTodo = () => {
-    this.setState({
-      todos: [...this.state.todos, {id: Math.random(), text: 'Novo Todo'}]
-    })
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
-        <View style={styles.box} />
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -54,7 +20,35 @@ const styles = StyleSheet.create({
     backgroundColor: '#F00',
     margin: 10,
     transform: [
-      { rotateZ: '20deg'}
-    ]
-  }
+      { rotateZ: '20deg' },
+    ],
+  },
 });
+
+
+export default class App extends Component {
+  state = {
+    todos: [
+      { id: 0, text: 'Fazer café' },
+      { id: 1, text: 'Estudar o GoNative' },
+    ],
+  }
+
+  addTodo = () => {
+    this.setState({
+      todos: [...this.state.todos, { id: Math.random(), text: 'Novo Todo' }],
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+      </View>
+    );
+  }
+}
