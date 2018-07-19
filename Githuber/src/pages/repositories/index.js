42d76356e7f02ca3c96e888ba16da24be53a4801
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-19 20:21:29 -0300

    Consumindo repos da API

--- Githuber/src/pages/repositories/index.js
+++ Githuber/src/pages/repositories/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import {
+  View, AsyncStorage, ActivityIndicator, FlatList, Text,
+} from 'react-native';
+import api from 'services/api';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+import styles from './styles';
 
 export default class Organizations extends Component {
   static navigationOptions = {
@@ -9,9 +14,47 @@ export default class Organizations extends Component {
     tabBarIcon: ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />,
   }
 
+  state = {
+    data: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const response = await api.get(`/users/${username}/repos`);
+
+    this.setState({ data: response.data, loading: false });
+  }
+
+  renderListItem = ({ item }) => (
+    <Text>
+      {item.full_name}
+    </Text>
+  );
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+    />
+  );
+
   render() {
     return (
-      <View />
+      <View style={styles.container}>
+
+        {
+        this.state.loading
+          ? <ActivityIndicator style={styles.loading} />
+          : this.renderList()
+      }
+
+      </View>
     );
   }
 }
