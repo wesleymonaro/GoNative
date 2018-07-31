7de7c2832f03221771d7094eb37cc7d6de91b585
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-30 21:10:20 -0300

    Configurando store

--- modulo03/src/index.js
+++ modulo03/src/index.js
@@ -1,34 +1,16 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/Entypo';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+
+import 'config/ReactotronConfig';
+import store from 'store';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Icon name="500px" size={20} />
-      </View>
+      <Provider store={store}>
+        <View />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
