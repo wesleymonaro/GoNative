fb9b0441c838a01dbc7ef7bdcddda5b7e022fa27
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-02 14:12:30 -0300

    Estilização do Favorito

--- modulo03/src/pages/favorites/components/favoriteItem/index.js
+++ modulo03/src/pages/favorites/components/favoriteItem/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import PropTypes from 'prop-types';
+
 import styles from './styles';
 
-const FavoriteItem = ({ favorite }) => (
+const FavoriteItem = ({ favorite: { name, owner, description } }) => (
   <View style={styles.container}>
-    <Text>
-      {favorite.full_name}
-    </Text>
+    <Image style={styles.avatar} source={{ uri: owner.avatar_url }} />
+    <View style={styles.info}>
+      <Text style={styles.title}>
+        {name}
+      </Text>
+      <Text numberOfLines={2} style={styles.description}>
+        {description}
+      </Text>
+    </View>
   </View>
 );
+
+FavoriteItem.propTypes = {
+  favorite: PropTypes.shape({
+    name: PropTypes.string,
+    description: PropTypes.string,
+    owner: PropTypes.shape({
+      avatar_url: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default FavoriteItem;
