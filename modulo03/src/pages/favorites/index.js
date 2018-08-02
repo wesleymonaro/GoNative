f16eb9e14512505c8727c74b8d7b414dc1d63c8a
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-02 15:31:44 -0300

    Listagem de favoritos

--- modulo03/src/pages/favorites/index.js
+++ modulo03/src/pages/favorites/index.js
@@ -1,155 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 import { View, Text, FlatList } from 'react-native';
 import styles from './styles';
 
 import FavoriteItem from './components/favoriteItem';
 
-export default class Favorites extends Component {
+class Favorites extends Component {
   static navigationOptions = {
     title: 'Meus Favoritos',
   };
 
-  state = {
-    favorites: [
-      {
-        "id": 10270250,
-        "node_id": "MDEwOlJlcG9zaXRvcnkxMDI3MDI1MA==",
-        "name": "react",
-        "full_name": "facebook/react",
-        "owner": {
-          "login": "facebook",
-          "id": 69631,
-          "node_id": "MDEyOk9yZ2FuaXphdGlvbjY5NjMx",
-          "avatar_url": "https://avatars3.githubusercontent.com/u/69631?v=4",
-          "gravatar_id": "",
-          "url": "https://api.github.com/users/facebook",
-          "html_url": "https://github.com/facebook",
-          "followers_url": "https://api.github.com/users/facebook/followers",
-          "following_url": "https://api.github.com/users/facebook/following{/other_user}",
-          "gists_url": "https://api.github.com/users/facebook/gists{/gist_id}",
-          "starred_url": "https://api.github.com/users/facebook/starred{/owner}{/repo}",
-          "subscriptions_url": "https://api.github.com/users/facebook/subscriptions",
-          "organizations_url": "https://api.github.com/users/facebook/orgs",
-          "repos_url": "https://api.github.com/users/facebook/repos",
-          "events_url": "https://api.github.com/users/facebook/events{/privacy}",
-          "received_events_url": "https://api.github.com/users/facebook/received_events",
-          "type": "Organization",
-          "site_admin": false
-        },
-        "private": false,
-        "html_url": "https://github.com/facebook/react",
-        "description": "A declarative, efficient, and flexible JavaScript library for building user interfaces.",
-        "fork": false,
-        "url": "https://api.github.com/repos/facebook/react",
-        "forks_url": "https://api.github.com/repos/facebook/react/forks",
-        "keys_url": "https://api.github.com/repos/facebook/react/keys{/key_id}",
-        "collaborators_url": "https://api.github.com/repos/facebook/react/collaborators{/collaborator}",
-        "teams_url": "https://api.github.com/repos/facebook/react/teams",
-        "hooks_url": "https://api.github.com/repos/facebook/react/hooks",
-        "issue_events_url": "https://api.github.com/repos/facebook/react/issues/events{/number}",
-        "events_url": "https://api.github.com/repos/facebook/react/events",
-        "assignees_url": "https://api.github.com/repos/facebook/react/assignees{/user}",
-        "branches_url": "https://api.github.com/repos/facebook/react/branches{/branch}",
-        "tags_url": "https://api.github.com/repos/facebook/react/tags",
-        "blobs_url": "https://api.github.com/repos/facebook/react/git/blobs{/sha}",
-        "git_tags_url": "https://api.github.com/repos/facebook/react/git/tags{/sha}",
-        "git_refs_url": "https://api.github.com/repos/facebook/react/git/refs{/sha}",
-        "trees_url": "https://api.github.com/repos/facebook/react/git/trees{/sha}",
-        "statuses_url": "https://api.github.com/repos/facebook/react/statuses/{sha}",
-        "languages_url": "https://api.github.com/repos/facebook/react/languages",
-        "stargazers_url": "https://api.github.com/repos/facebook/react/stargazers",
-        "contributors_url": "https://api.github.com/repos/facebook/react/contributors",
-        "subscribers_url": "https://api.github.com/repos/facebook/react/subscribers",
-        "subscription_url": "https://api.github.com/repos/facebook/react/subscription",
-        "commits_url": "https://api.github.com/repos/facebook/react/commits{/sha}",
-        "git_commits_url": "https://api.github.com/repos/facebook/react/git/commits{/sha}",
-        "comments_url": "https://api.github.com/repos/facebook/react/comments{/number}",
-        "issue_comment_url": "https://api.github.com/repos/facebook/react/issues/comments{/number}",
-        "contents_url": "https://api.github.com/repos/facebook/react/contents/{+path}",
-        "compare_url": "https://api.github.com/repos/facebook/react/compare/{base}...{head}",
-        "merges_url": "https://api.github.com/repos/facebook/react/merges",
-        "archive_url": "https://api.github.com/repos/facebook/react/{archive_format}{/ref}",
-        "downloads_url": "https://api.github.com/repos/facebook/react/downloads",
-        "issues_url": "https://api.github.com/repos/facebook/react/issues{/number}",
-        "pulls_url": "https://api.github.com/repos/facebook/react/pulls{/number}",
-        "milestones_url": "https://api.github.com/repos/facebook/react/milestones{/number}",
-        "notifications_url": "https://api.github.com/repos/facebook/react/notifications{?since,all,participating}",
-        "labels_url": "https://api.github.com/repos/facebook/react/labels{/name}",
-        "releases_url": "https://api.github.com/repos/facebook/react/releases{/id}",
-        "deployments_url": "https://api.github.com/repos/facebook/react/deployments",
-        "created_at": "2013-05-24T16:15:54Z",
-        "updated_at": "2018-08-02T16:51:18Z",
-        "pushed_at": "2018-08-02T16:37:45Z",
-        "git_url": "git://github.com/facebook/react.git",
-        "ssh_url": "git@github.com:facebook/react.git",
-        "clone_url": "https://github.com/facebook/react.git",
-        "svn_url": "https://github.com/facebook/react",
-        "homepage": "https://reactjs.org",
-        "size": 137389,
-        "stargazers_count": 107820,
-        "watchers_count": 107820,
-        "language": "JavaScript",
-        "has_issues": true,
-        "has_projects": true,
-        "has_downloads": true,
-        "has_wiki": true,
-        "has_pages": true,
-        "forks_count": 19234,
-        "mirror_url": null,
-        "archived": false,
-        "open_issues_count": 462,
-        "license": {
-          "key": "mit",
-          "name": "MIT License",
-          "spdx_id": "MIT",
-          "url": "https://api.github.com/licenses/mit",
-          "node_id": "MDc6TGljZW5zZTEz"
-        },
-        "forks": 19234,
-        "open_issues": 462,
-        "watchers": 107820,
-        "default_branch": "master",
-        "organization": {
-          "login": "facebook",
-          "id": 69631,
-          "node_id": "MDEyOk9yZ2FuaXphdGlvbjY5NjMx",
-          "avatar_url": "https://avatars3.githubusercontent.com/u/69631?v=4",
-          "gravatar_id": "",
-          "url": "https://api.github.com/users/facebook",
-          "html_url": "https://github.com/facebook",
-          "followers_url": "https://api.github.com/users/facebook/followers",
-          "following_url": "https://api.github.com/users/facebook/following{/other_user}",
-          "gists_url": "https://api.github.com/users/facebook/gists{/gist_id}",
-          "starred_url": "https://api.github.com/users/facebook/starred{/owner}{/repo}",
-          "subscriptions_url": "https://api.github.com/users/facebook/subscriptions",
-          "organizations_url": "https://api.github.com/users/facebook/orgs",
-          "repos_url": "https://api.github.com/users/facebook/repos",
-          "events_url": "https://api.github.com/users/facebook/events{/privacy}",
-          "received_events_url": "https://api.github.com/users/facebook/received_events",
-          "type": "Organization",
-          "site_admin": false
-        },
-        "network_count": 19234,
-        "subscribers_count": 6183
-      }
-    ]
+  static propTypes = {
+    favorites: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+    })).isRequired,
   }
 
   renderList = () => (
     <FlatList
-      data={this.state.favorites}
+      data={this.props.favorites}
       keyExtractor={item => String(item.id)}
-      renderItem={({item}) => <FavoriteItem favorite={item} />}
+      renderItem={({ item }) => <FavoriteItem favorite={item} />}
     />
   );
 
   render() {
     return (
       <View style={styles.container}>
-        { !this.state.favorites.length
-          ? <Text style={styles.empty}>Nenhum favorito adicionado</Text>
+        { !this.props.favorites.length
+          ? (
+            <Text style={styles.empty}>
+              Nenhum favorito adicionado
+            </Text>
+          )
           : this.renderList() }
       </View>
     );
   }
-};
+}
+
+const mapStateToProps = state => ({
+  favorites: state.favorites,
+});
+
+export default connect(mapStateToProps)(Favorites);
