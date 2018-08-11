0f30a5697e7a9bb8d6b6fa3a1be0055f38cec617
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-11 14:12:07 -0300

    Estilizando detalhe do álbum

--- modulo04/src/pages/album/index.js
+++ modulo04/src/pages/album/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import {
+  View, ImageBackground, Text, FlatList,
+} from 'react-native';
+import SongItem from 'components/songItem';
 
-// import styles from './styles';
+import styles from './styles';
 
-const Album = () => (
-  <View />
-);
+const Album = ({ navigation }) => {
+  const { album } = navigation.state.params;
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.thumbnail}
+        blurRadius={5}
+        source={{ uri: album.thumbnail }}
+      >
+        <View style={styles.thumbnailContainer}>
+          <Text style={styles.title}>
+            {album.title}
+          </Text>
+          <Text style={styles.author}>
+            {album.author}
+          </Text>
+        </View>
+      </ImageBackground>
+
+      <FlatList
+        data={album.songs}
+        keyExtractor={song => String(song.id)}
+        renderItem={({ item }) => <SongItem song={item} />}
+      />
+    </View>
+  );
+};
 
 Album.navigationOptions = ({ navigation }) => ({
   title: navigation.state.params.album.title,
